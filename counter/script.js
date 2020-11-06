96d202bc863eaf37956ea330669a9e784d6774bc
MichealRivas17 <67254542+MichealRivas17@users.noreply.github.com>
2020-11-07 06:57:51 +0800

    Add files via upload

---
+++ counter/script.js
@@ -0,0 +1,30 @@
+(function() {
+    //set the counter
+    const buttons = document.querySelectorAll('.counterBtn')
+    let count = 0
+    
+
+    //add functionality on each button
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            if (button.classList.contains('prevBtn')) {
+                count--
+            } else if (button.classList.contains('nextBtn')) {
+                count++
+            }
+
+            //Select the counter text
+            const counter = document.querySelector('#counter')
+            counter.textContent = count
+
+            if (count > 0) {
+                counter.style.color = 'green'
+            } else if (count < 0){
+                counter.style.color = 'red'
+            } else {
+                counter.style.color = 'black'
+            }
+
+        })
+    })
+})();
